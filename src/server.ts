787aed9db5c1e455ce832aa5f280f6994ccfaed8
Eric Cheng <online.eric@gmail.com>
2024-10-07 22:58:08 +1300

    add env var

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import app from './app';
+import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+export const ENV_VAR_PREFIX: string = "EXPRESS_";
+export const getPrefixedEnvVar = (key: string): string | undefined => {
+	return process.env[`${ENV_VAR_PREFIX}${key}`];
+};
+
+const PORT = getPrefixedEnvVar("PORT") || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	if (getPrefixedEnvVar("NODE_ENV") === "development") {
+		console.log(`Server is running on http://localhost:${PORT}`);
+	}
 });
