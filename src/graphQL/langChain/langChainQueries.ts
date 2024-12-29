ebac38ff0eed6d1baf310902024bb693520add34
Eric Cheng <online.eric@gmail.com>
2024-12-30 08:48:09 +1300

    calling openAI api using langChain

--- src/graphQL/langChain/langChainQueries.ts
+++ src/graphQL/langChain/langChainQueries.ts
@@ -1,34 +1,42 @@
 import { GetResponseInput, GetResponseOutput } from "./langChainDefs";
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
 
-export const getLangChainResponse = (
+const chain = new StringOutputParser();
+
+export const getLangChainResponse = async (
 	_: any,
-	{ input } : { input: GetResponseInput }
-): GetResponseOutput => {
-	console.log("user input: ", input.userInput);
+	{ input }: { input: GetResponseInput }
+): Promise<GetResponseOutput> => {
+	// convert chat history format from [{role: string, message: string}] to [[string, string]]
+	const chatHistory: [string, string][] = input.chatHistory.map((chat) => [
+		chat.role,
+		chat.message,
+	]);
+	chatHistory.push(["user", input.userInput]);
+
+	const prompt = ChatPromptTemplate.fromMessages(chatHistory);
+
+	const chatModelOpenAI = new ChatOpenAI({
+		apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+		model: input.dataModel,
+		temperature: input.temperature,
+	});
+
+	// Link the updated prompt with the ChatOpenAI model
+	const chatRes = await prompt.pipe(chatModelOpenAI).pipe(chain).invoke({
+		input: input.userInput,
+	});
+
+	// Append the bot's response to the chat history
+	chatHistory.push(["ai", chatRes]);
+
+	// convert chat history format back to [{role: string, message: string}]
+	const chatHistoryOutput = chatHistory.map(([role, message]) => ({role, message}));
+
 	return {
-		modelResponse: "Model Response",
-		chatHistory: [
-			{
-				role: "system",
-				message:
-					"You are Eric Cheng, a senior full stack developer at a tech company.",
-			},
-			{
-				role: "system",
-				message:
-					"You are demonstrating your AI development skills to potential employers.",
-			},
-			{
-				role: "system",
-				message:
-					"You have solid skills and experience in React js, C# and SQL DB development.",
-			},
-			{
-				role: "system",
-				message:
-					"You are very interested in AI development and have been learning about RAG and data models.",
-			},
-			{ role: "user", message: "What is RAG?" },
-		],
+		modelResponse: chatRes,
+		chatHistory: chatHistoryOutput,
 	} as GetResponseOutput;
 };
